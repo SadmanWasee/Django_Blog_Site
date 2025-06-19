016a2ec7eb22ed215aea34eed9f211161fb48281
Sadman Wasee <sadman.wasee125@gmail.com>
2025-06-19 17:39:56 +0600

    Added the forms, views and url

--- Blog_Site/Blog_Site/settings.py
+++ Blog_Site/Blog_Site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Blog_Site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Blog_Site/Blogs/views.py
+++ Blog_Site/Blogs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import Post
+from .forms import PostForm
 # Create your views here.
+
+class IndexView(View):
+    
+    def get(self,request):
+        latest_posts = Post.objects.all().order_by("-date")[:3]
+        return render(request, "index.html", {
+            "posts":latest_posts
+        })
